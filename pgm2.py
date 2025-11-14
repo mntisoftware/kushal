ad25f6e71008c7570f7a9b389698ba6d802b3505
mntisoftware <mnitsoftware1234@gmail.com>
2025-11-14 12:28:55 +0530

    python

---
+++ pgm2.py
@@ -0,0 +1,11 @@
+import numpy as np 
+a=np.array([1,2,3,4,5])
+print(a)
+print(np.sum(a))
+print(np.mean(a))
+print(np.min(a))
+print(np.max(a))
+print(np.argmin(a))
+print(np.argmax(a))
+print(np.std(a))
+print(np.var(a))
